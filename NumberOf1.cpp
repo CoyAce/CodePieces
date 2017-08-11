0088d241bac5ba0f961d3bbf9a0835229a11f339
Coy <coylook@outlook.com>
2017-08-11 14:45:51 +0800

    Update NumberOf1.cpp

--- NumberOf1.cpp
+++ NumberOf1.cpp
@@ -1,12 +1,12 @@
-int  NumberOf1(int n) {
-	int cnt = 8 * sizeof(int);
-	int tmp = 0;
-	while (cnt != 0)
-	{
-		if (n & 1)
-			tmp++;
-		n = n >> 1;
-		cnt--;
-	}
-	return tmp;
-}
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int count=0;
+         while(n)
+         {
+             count++;
+             n&=n-1;
+         }
+         return count;
+     }
+};
